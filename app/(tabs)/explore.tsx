1d526d45cd1eb8af07d69473a1e5d4d7e5cfacbe
Jonas_Gertz <Jonas.Gertz@enfogroup.com>
2024-08-14 20:08:02 +0200

    working on phone... finally

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,61 +1,108 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
+import { StyleSheet, Image, Pressable } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useState, useEffect } from 'react';
+import * as cities from '../../data/geonames-all-cities-with-a-population-1000.json'
+import { useDay } from '@/@contexts/day-context';
+import { router } from 'expo-router';
+import React from 'react';
+import Dropdown from '@/components/Dropdown';
+import * as Location from 'expo-location';
 
 export default function TabTwoScreen() {
+  const [data, setData] = useState<any>(null)
+  const [defaultLocation, setDefaultLocation] = useState<any | null>(null)
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const { day, setDay } = useDay();
+  const [selectedOption, setSelectedOption] = useState("");
+  const [typedOptions, setTypedOptions] = useState<any[] | null>(null);
+
+  
+;  // const options = typedOptions.sort((a, b) => a?.name?.localeCompare(b?.name))?.map((city: any) => city?.name)
+  const handleOptionSelected = (option: any) => {
+    setSelectedOption(option);
+    fetchWeather(option)
+  }
+
+  const fetchWeather = async (searchValue = "Stockholm") => {
+    try {
+      const res = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c08645764b994410956135425243007&q=${searchValue}&days=7&aqi=no&alerts=no`)
+      const json = await res.json()
+      const allDays = json?.forecast?.forecastday
+
+      setData(allDays)
+      setDefaultLocation(json?.location?.name)
+      return res
+    } catch (error) {
+      return error
+    }
+  }
+
+  function changeDate(date: string) {
+    setDay({day: date})
+    router.navigate('/')
+  }
+
+  useEffect(() => {
+    if (cities) {
+      setTypedOptions(cities)
+    }
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      const latitude = location.coords.latitude;
+      const longitude = location.coords.longitude;
+      const text = `${latitude},${longitude}` 
+      
+      setDefaultLocation(location);
+      fetchWeather(text)
+      .catch(console.error);
+    })();
+  }, [cities]);
+
+  const getTime = (date: string) => {
+    return date.split(' ')?.[1] || date
+  }
+
   const weatherType = 'sunny'
   const image = weatherType === 'sunny' ? 'sunny.jpg' : 'rain.jpg'
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-        source={require(`@/assets/images/${image}`)}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Today: 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Wednesday: 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Thursday: 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Friday: 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Saturday: 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Sunday: 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-          Monday: 
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    typedOptions && defaultLocation && data && data?.length > 0 && <ParallaxScrollView
+       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+       headerImage={
+         <Image
+           source={require(`@/assets/images/${image}`)}
+           style={styles.reactLogo}
+         />
+       }>
+       <Dropdown
+        options={typedOptions}
+        onOptionSelected={handleOptionSelected}
+        defaultLocation={defaultLocation}
+        placeholder={"Search.."}
+      />
+       {data.map((day: any) => (
+         <ThemedView key={day.date}>
+          <Pressable onPress={() => changeDate(day.date)}>
+          <ThemedText style={styles.stepContainer}>{ getTime(day?.date) }
+          <ThemedText> {day?.day?.avgtemp_c } °</ThemedText>
+          <Image
+            source={{uri: day?.day?.condition?.icon}}
+            style={styles.weatherIcon}
+          />
+          </ThemedText>
+          </Pressable>
+        </ThemedView>
+       ))}
+     </ParallaxScrollView> ||  <ThemedView><ThemedText>2ndpage</ThemedText></ThemedView>
+   );
 }
 
 
@@ -63,9 +110,11 @@ const styles = StyleSheet.create({
   stepContainer: {
     borderBottomWidth: 0.2,
     borderBottomColor: 'red',
-    gap: 8,
-    paddingBottom: 16,
-    marginBottom: 8,
+    gap: 16,
+    paddingBottom: 8,
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row'
   },
   reactLogo: {
     height: '100%',
@@ -73,5 +122,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  weatherIcon: {
+    width: 32,
+    height: 32
   }
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,69 +1,60 @@
-import { Text, Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useEffect, useState } from 'react';
+import * as cities from '../../data/geonames-all-cities-with-a-population-1000.json'
+import { useDay } from '@/@contexts/day-context';
+import React from 'react';
+import Dropdown from '@/components/Dropdown';
+import * as Location from 'expo-location';
 
 export default function HomeScreen() {
   const [data, setData] = useState<any>(null)
+  const [currentDay, setCurrentDay] = useState<any>(null)
   const [restOfDay, setRestOfDay] = useState<any[] | null>(null)
+  const [defaultLocation, setDefaultLocation] = useState<any | null>(null)
+  const { day, setDay } = useDay();
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [typedOptions, setTypedOptions] = useState<any[] | null>(null);
 
+  const [selectedOption, setSelectedOption] = useState("");
+  // const typedOptions = cities as any[]
+  // const options = typedOptions.sort((a, b) => a.name.localeCompare(b.name)).map((city: any) => city.name)
+  const handleOptionSelected = (option: any) => {
+    setSelectedOption(option);
+    fetchWeather(option)
+  };
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        const res = await fetch(`https://api.weatherapi.com/v1/forecast.json?q=Stockholm&days=1&alerts=no&aqi=no&key=c08645764b994410956135425243007`)
-        const json = await res.json()
-        // console.log(json);
-        
-        const allTimes = json?.forecast?.forecastday?.[0].hour
-        const currentTime = json.current?.last_updated_epoch
-        const timesOfInterest = allTimes.filter((time: any) => time.time_epoch > currentTime)
-        setRestOfDay(timesOfInterest)
-        
-        setData(json)
-        return res
-      } catch (error) {
-        return error
-      }
+  const fetchWeather = async (searchValue?: string) => {
+    try {
+      const res = await fetch(`https://api.weatherapi.com/v1/forecast.json?q=${searchValue}&days=7&alerts=no&aqi=no&key=c08645764b994410956135425243007`)
+      const json = await res.json()
 
+      const allTimes = json?.forecast?.forecastday?.[0].hour
+      const currentTime = json.current?.last_updated_epoch
+      const timesOfInterest = allTimes.filter((time: any) => time.time_epoch > currentTime)
+      setRestOfDay(timesOfInterest)
+      setData(json)
+      return res
+    } catch (error) {
+      return error
     }
-
-    fetchWeather()
-    .catch(console.error);
-  }, [])
-
-  const getTime = (date: string) => {
-    return date.split(' ')?.[1] || date
   }
 
-  const weatherType = 'sunny'
-  const image = weatherType === 'sunny' ? 'sunny.jpg' : 'rain.jpg'
-  
-  return (
-   data && restOfDay && restOfDay?.length > 0 && <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require(`@/assets/images/${image}`)}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText>
-        {data?.location?.name ?? ''} 
-        </ThemedText>
-      </ThemedView>
-      <ThemedView >
-        <ThemedText style={styles.stepContainer}>
+  function renderData() {
+    if (data && restOfDay && restOfDay?.length > 0 ) {
+      return <>
+      <ThemedView>
+      <ThemedText style={styles.stepContainer}> 
         { getTime(data?.current?.last_updated) }
         <ThemedText> {data?.current?.temp_c } °</ThemedText>
         <Image
           source={{uri: data?.current?.condition?.icon}}
           style={styles.weatherIcon}
         />
-        </ThemedText>
-      </ThemedView>
+      </ThemedText>
+    </ThemedView>
       {restOfDay.map((day) => (
         <ThemedView key={day.time}>
         <ThemedText style={styles.stepContainer}>
@@ -76,12 +67,78 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       ))}
-    </ParallaxScrollView>
-    ||
-    <View>
-      <Text>Loading data...</Text>
-    </View>
+    </>
+    }
+    return null;
+  }
 
+
+  useEffect(() => {
+    if (cities) {
+      setTypedOptions(cities)
+    }
+    if (!data) {
+      (async () => {
+        
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+  
+        let location = await Location.getCurrentPositionAsync({});
+        const latitude = location.coords.latitude;
+        const longitude = location.coords.longitude;
+        const text = `${latitude},${longitude}` 
+        
+        setDefaultLocation(location);
+        fetchWeather(text)
+        .catch(console.error);
+      })();
+    }
+    if (day?.day) {
+      setCurrentDay(day.day)
+      if (day.day !== currentDay) {
+        if (!data) {
+          fetchWeather(selectedOption)
+        } else {
+          setRestOfDay([])
+          const selectedDay = data?.forecast?.forecastday?.find((dayDate: any) => dayDate.date === day?.day)
+          const allTimes = selectedDay.hour
+          const currentTime = data.current?.last_updated.split(' ')?.[1]
+          const timesOfInterest = allTimes.filter((time: any) => time.time.split(' ')?.[1] > currentTime)
+          setRestOfDay(timesOfInterest)
+        }
+      }
+    }
+  }, [currentDay, data, selectedOption, day, cities])
+
+  const getTime = (date: string) => {
+    return date.split(' ')?.[1] || date
+  }
+
+  const weatherType = 'sunny'
+  const image = weatherType === 'sunny' ? 'sunny.jpg' : 'rain.jpg'
+  
+  return (
+   <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require(`@/assets/images/${image}`)}
+          style={styles.reactLogo}
+        />
+      }>
+       <Dropdown
+        options={typedOptions}
+        onOptionSelected={handleOptionSelected}
+        defaultLocation={defaultLocation}
+        placeholder={"Search.."}
+      />
+      {renderData() }
+
+
+    </ParallaxScrollView> || <ThemedView><ThemedText>Hello</ThemedText></ThemedView>
   );
 }
 
