14262ad67ca9558e86f40d3b2060597401fab20b
Jonas Gertz <jonas.gertz@epicalgroup.com>
2024-08-15 11:09:33 +0200

    add context

--- @contexts/day-context.tsx
+++ @contexts/day-context.tsx
@@ -16,10 +16,25 @@ type DayContextType = {
 
 const DayContext = createContext<DayContextType | undefined>(undefined);
 
+type CitiesContextType = {
+  cities: { [key: string]: any } | null;
+  setCities: Dispatch<SetStateAction<{ [key: string]: any } | null>>;
+};
+
+const CitiesContext = createContext<CitiesContextType | undefined>(undefined);
+
 function useDay(): DayContextType {
   const context = useContext(DayContext);
   if (!context) {
       throw new Error("useDay must be used within an DayProvider");
+  }
+  return context;
+}
+
+function useCities(): CitiesContextType {
+  const context = useContext(CitiesContext);
+  if (!context) {
+      throw new Error("useCities must be used within an CitiesProvider");
   }
   return context;
 }
@@ -30,4 +45,9 @@ const DayProvider = (props: { children: ReactNode }): ReactElement => {
   return <DayContext.Provider {...props} value={{ day, setDay }} />;
 };
 
-export { DayProvider, useDay };
+const CitiesProvider = (props: { children: ReactNode }): ReactElement => {
+  const [cities, setCities] = useState<{ [key: string]: any } | null>(null);
+
+  return <CitiesContext.Provider {...props} value={{ cities, setCities }} />;
+};
+export { DayProvider, useDay, CitiesProvider, useCities };
